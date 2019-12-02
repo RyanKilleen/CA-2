deb53a40ee555f56bfc15179a7678ae38b5394b9
S00188061 <S00188061@LAB-B1041-20>
2019-12-02 12:09:55 +0000

    Bike And Activity Clases added

---
+++ CA 2/Bike.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_2
+{
+    public enum Gender {Male, Female }
+     public class Bike
+    {
+        //Setting The Properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Gender SuitableFor { get; set; }
+
+        //Setting Up the Constructors
+        public Bike(int id, string name, decimal price, Gender gender)
+        {
+            ID = id;
+            Name = name;
+            Price = price;
+            SuitableFor = gender;
+        }
+
+        //Creating The Methods
+        public override string ToString()
+        {
+            return $"{Name} - {Price:C}";
+        }
+
+
+    }
+}
--- CA 2/MainWindow.xaml.cs
+++ CA 2/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace CA_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Bike> allBikes = new List<Bike>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Making Some Bike Objects
+            Bike aProduct = new Bike(1234, "Road Bike", 535.00m, Gender.Male);
+            Bike bProduct = new Bike(1235, "Road Bike", 635.00m, Gender.Female);
+            Bike cProduct = new Bike(1236, "Mountain Bike", 740.00m, Gender.Male);
+            Bike dProduct = new Bike(1237, "Mountain Bike", 780.00m, Gender.Female);
+            Bike eProduct = new Bike(1238, "Hybrid Bike", 155.00m, Gender.Male);
+            Bike fProduct = new Bike(1239, "Hybrid Bike", 150.00m, Gender.Female);
+
+            //Adding to List
+            allBikes.Add(aProduct);
+            allBikes.Add(bProduct);
+            allBikes.Add(cProduct);
+            allBikes.Add(dProduct);
+            allBikes.Add(eProduct);
+            allBikes.Add(fProduct);
+
+            //Displying Them in The ListBox
+            //This tells the ListBox That the source of the items is the List
+            lbxProducts.ItemsSource = allBikes;
+
+
+
         }
     }
 }
